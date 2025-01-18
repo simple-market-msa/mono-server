8bc4bdc7b96ac7a299e3e5e5c8f1bcdf8ac815a1
임채연 <dlacodus0407@gmail.com>
2025-01-18 21:02:16 +0900

    init

---
+++ src/main/java/com/isyoudwn/simplemarketmono/SimpleMarketMonoApplication.java
@@ -0,0 +1,13 @@
+package com.isyoudwn.simplemarketmono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMarketMonoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleMarketMonoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isyoudwn/simplemarketmono/SimpleMarketMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isyoudwn.simplemarketmono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMarketMonoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
